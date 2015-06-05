40bc4a5609d9d9086b56684203a4d466bef1fbfc
amarkin <alex.markin57@gmail.com>
2015-06-05 23:33:28 +0300

    fixed the setup configuration and created the console entry point

--- setup.py
+++ setup.py
@@ -8,15 +8,21 @@ setuptools.setup(
 
     packages=setuptools.find_packages(),
     install_requires=[
-        'numpy>=1.6.1',
-        'scipy>=0.9'
-        'scikit-learn>=0.16.1',
         'east>=0.3.1',
-        'enum34>=1.0.4'
+        'enum34>=1.0.4',
+        'lxml',
+        'networkx'
     ],
 
     extras_require={
-        'graphs':  ['nodebox-opengl', 'pyglet']
+        'graphs':  ['nodebox-opengl', 'pyglet'],
+        'spectral_coclustering': ['numpy>=1.6.1', 'scipy>=0.9', 'scikit-learn>=0.16.1']
+    },
+
+    entry_points={
+        "console_scripts": [
+            "bianalyzer = bianalyzer.main:main"
+        ]
     },
 
     author='Alexey Markin',
