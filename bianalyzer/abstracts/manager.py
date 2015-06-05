ccbbf3c033b6dfd9d4875e884a014abb29ada255
amarkin <alex.markin57@gmail.com>
2015-06-05 23:12:22 +0300

    changed the API key policy

--- bianalyzer/abstracts/manager.py
+++ bianalyzer/abstracts/manager.py
@@ -8,12 +8,15 @@ from .article import Article
 SUPPORTED_SOURCE_LIST = ['IEEE', 'Springer']
 
 
-def download_abstracts(source, raw_query, number):
+def download_abstracts(source, raw_query, number, springer_api_key=None):
     if number > DownloadingConfig.MAX_ARTICLES_PER_QUERY:
         raise InvalidArgumentError('number', number, message='cannot handle more than %s articles at once'
                                                              % DownloadingConfig.MAX_ARTICLES_PER_QUERY)
     if source not in SUPPORTED_SOURCE_LIST:
-            raise InvalidArgumentError('source', source, 'Supported sources are %s' % SUPPORTED_SOURCE_LIST)
+        raise InvalidArgumentError('source', source, 'Supported sources are %s' % SUPPORTED_SOURCE_LIST)
+    if source == 'Springer' and springer_api_key is None:
+        raise InvalidArgumentError('springer_api_key', springer_api_key, 'You should indicate the API Key '
+                                                                         'in order to download articles from Springer')
 
     raw_query = raw_query.strip()
 
@@ -26,6 +29,8 @@ def download_abstracts(source, raw_query, number):
         while left_number > 0:
             real_number = left_number if left_number < max_records else max_records
             new_records = download_ieee_abstracts(raw_query, real_number, start_index=start_index)
+            if new_records == 0 and len(records) == 0:
+                raise Exception('Could not start downloading the abstracts')
             records.extend(new_records)
             left_number -= len(new_records)
             start_index += len(new_records)
@@ -33,7 +38,9 @@ def download_abstracts(source, raw_query, number):
         max_records = DownloadingConfig.SPRINGER_MAX_RECORDS
         while left_number > 0:
             real_number = left_number if left_number < max_records else max_records
-            new_records = download_springer_abstracts(raw_query, real_number, start_index=start_index)
+            new_records = download_springer_abstracts(raw_query, real_number, springer_api_key, start_index=start_index)
+            if new_records == 0 and len(records) == 0:
+                raise Exception('Could not start downloading the abstracts')
             records.extend(new_records)
             left_number -= len(new_records)
             start_index += len(new_records)
