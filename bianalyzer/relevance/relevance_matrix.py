ef25ee0944d04088da4ee724ccb07e81447c04d5
amarkin <alex.markin57@gmail.com>
2015-06-05 23:22:16 +0300

    changed the metrics' names and added the 'query=False' parameter for bm25 and tf-idf metrics; fixed the tf-idf metric: it does not consider the document's length anymore

--- bianalyzer/relevance/relevance_matrix.py
+++ bianalyzer/relevance/relevance_matrix.py
@@ -7,28 +7,40 @@ from .relevance_metrics import tf_idf, bm25, keyword_frequency
 
 
 class RelevanceMatrix:
-    def __init__(self, keywords, texts, matrix, max_score):
+    def __init__(self, keywords, texts, matrix, max_score=None):
         self.keywords = keywords
         self.texts = texts
         self.matrix = matrix
         self.max_relevance_score = max_score
+        if max_score is None:
+            self._find_max_score()
+
+    def _find_max_score(self):
+        max_score = -100000
+        for row in self.matrix:
+            for val in row:
+                if val > max_score:
+                    max_score = max_score
+        self.max_relevance_score = max_score
 
 
 class RelevanceMetric:
-    tf_idf = "TF IDF"
-    bm25 = "BM25"
-    ast = "AST"
-    naive = "Naive"
+    tf_idf = "tf-idf"
+    bm25 = "bm25"
+    ast = "ast"
+    frequency = "frequency"
+    normalized_frequency = "normalized_frequency"
 
 relevance_metrics = (
     RelevanceMetric.tf_idf,
     RelevanceMetric.bm25,
     RelevanceMetric.ast,
-    RelevanceMetric.naive
+    RelevanceMetric.frequency,
+    RelevanceMetric.normalized_frequency
 )
 
 
-def construct_relevance_matrix(keywords, bianalyzer_texts, relevance_metric, params=None):
+def construct_relevance_matrix(keywords, bianalyzer_texts, relevance_metric, query=False, params=None):
     if relevance_metric not in relevance_metrics:
         raise InvalidArgumentError("relevance_metric", relevance_metric, "Such relevance metric is not supported")
     if params is not None and not isinstance(params, dict):
@@ -40,7 +52,7 @@ def construct_relevance_matrix(keywords, bianalyzer_texts, relevance_metric, par
     average_doc_length = calculate_average_text_length(bianalyzer_texts)
     print 'average text length: %s' % average_doc_length
 
-    # ast only
+    # AST only
     text_asts = []
     if relevance_metric == RelevanceMetric.ast:
         for bianalyzer_text in bianalyzer_texts:
@@ -55,25 +67,43 @@ def construct_relevance_matrix(keywords, bianalyzer_texts, relevance_metric, par
     relevance_matrix = []
     max_score = 0
     for (i, keyword) in enumerate(keywords):
+        kw_words = keyword.split()
         relevance_matrix.append([])
+
+        # TF-IDF and BM25 preparation (for IDF calculation)
         if relevance_metric == RelevanceMetric.tf_idf or relevance_metric == RelevanceMetric.bm25:
-            kw_occurrences = 0
+            kw_occurrences = 0  # frequency as a whole
+            kw_word_occurrences = [0 for _ in range(len(kw_words))]  # frequency for each word separately
             for bianalyzer_text in bianalyzer_texts:
                 if keyword_frequency(bianalyzer_text, keyword) > 0:
                     kw_occurrences += 1
+                for j, word in enumerate(kw_words):
+                    if keyword_frequency(bianalyzer_text, word) > 0:
+                        kw_word_occurrences[j] += 1
 
         for (j, bianalyzer_text) in enumerate(bianalyzer_texts):
             relevance_score = 0
             if relevance_metric == RelevanceMetric.tf_idf:
-                relevance_score = tf_idf(collection_length, kw_occurrences,
-                                         bianalyzer_text, keyword)
+                if query:
+                    for k, word in enumerate(kw_words):
+                        relevance_score += tf_idf(word, collection_length, kw_word_occurrences[k], bianalyzer_text)
+                else:
+                    relevance_score = tf_idf(keyword, collection_length, kw_occurrences,
+                                             bianalyzer_text)
             elif relevance_metric == RelevanceMetric.bm25:
-                relevance_score = bm25(collection_length, kw_occurrences,
-                                       average_doc_length, bianalyzer_text, keyword)
+                if query:
+                    for k, word in enumerate(kw_words):
+                        relevance_score += bm25(word, collection_length, kw_word_occurrences[k], average_doc_length,
+                                                bianalyzer_text)
+                else:
+                    relevance_score = bm25(keyword, collection_length, kw_occurrences,
+                                           average_doc_length, bianalyzer_text)
             elif relevance_metric == RelevanceMetric.ast:
                 if keyword_frequency(bianalyzer_text, keyword) > 0.0 and text_asts[j] is not None:
                     relevance_score = text_asts[j].score(keyword.upper())
-            elif relevance_metric == RelevanceMetric.naive:
+            elif relevance_metric == RelevanceMetric.frequency:
+                relevance_score = float(keyword_frequency(bianalyzer_text, keyword))
+            elif relevance_metric == RelevanceMetric.normalized_frequency:
                 relevance_score = float(keyword_frequency(bianalyzer_text, keyword)) / bianalyzer_text.text_length
 
             if relevance_score > max_score:
@@ -81,6 +111,7 @@ def construct_relevance_matrix(keywords, bianalyzer_texts, relevance_metric, par
 
             relevance_matrix[i].append(relevance_score)
 
+    print 'max relevance score in the matrix: %.3f' % max_score
     return RelevanceMatrix(keywords, bianalyzer_texts, relevance_matrix, max_score)
 
 
