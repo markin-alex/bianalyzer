f7bd730593075a32c3a2bbbde1036216547da86a
amarkin <alex.markin57@gmail.com>
2015-06-07 22:38:46 +0300

    added control of nodebox-opengl importing

--- bianalyzer/main.py
+++ bianalyzer/main.py
@@ -7,7 +7,6 @@ from bianalyzer.abstracts import download_abstracts
 from bianalyzer.biclustering import get_keyword_biclusters, GreedyBBox, get_keyword_text_biclusters, \
     save_keyword_text_biclusters
 from bianalyzer.biclustering.keywords_analysis import save_keyword_biclusters
-from bianalyzer.graphs import construct_keyword_graph, draw_keyword_biclusters
 from bianalyzer.keywords import extract_keywords_via_textrank
 from bianalyzer.relevance import construct_relevance_matrix, construct_similarity_matrix
 
@@ -40,7 +39,7 @@ def main():
     if opts['-d'] == '':
         opts['-d'] = True
 
-    print opts, args
+    # print opts, args
 
     if opts['-d'] and opts['-r']:
         print_error('Cannot use both options -r and -d simultaneously')
@@ -92,8 +91,13 @@ def main():
             keyword_biclusters = get_keyword_biclusters(similarity_matrix, GreedyBBox)
             save_keyword_biclusters(keyword_biclusters, biclusters_file)
             if opts['-d']:
-                edges = construct_keyword_graph(keyword_biclusters.biclusters)
-                draw_keyword_biclusters(edges)
+                try:
+                    from bianalyzer.graphs import construct_keyword_graph, draw_keyword_biclusters
+                    edges = construct_keyword_graph(keyword_biclusters.biclusters)
+                    draw_keyword_biclusters(edges)
+                except Exception:
+                    print '-------------------------'
+                    print 'Could not draw the graph! Please, install the nodebox-opengl package'
         else:
             keyword_text_biclusters = get_keyword_text_biclusters(relevance_matrix, GreedyBBox)
             save_keyword_text_biclusters(keyword_text_biclusters, biclusters_file)
